d6fe76bc0caee6eadf868f36732ee1fa61400454
Egortk <40430931+Egortk@users.noreply.github.com>
2021-02-04 23:23:42 +0200

    готово

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,43 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import AddReview from "../add-review/add-review";
+import MoviePage from "../movie-page/movie-page";
+import MyList from "../my-list/my-list";
+import NotFoundPage from "../not-found-page/not-found-page";
+import Player from "../player/player";
+import SignIn from "../sign-in/sign-in";
 import MainScreen from "../main-screen/main-screen";
 
 const App = (props) => {
   const {movies} = props;
-  return <MainScreen movies={movies} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen movies={movies} />;
+        </Route>
+        <Route exact path="/movie/:id">
+          <MoviePage />;
+        </Route>
+        <Route exact path="/movie/:id/review">
+          <AddReview />;
+        </Route>
+        <Route exact path="/my-list">
+          <MyList />;
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />;
+        </Route>
+        <Route exact path="/sign-in">
+          <SignIn />;
+        </Route>
+        <Route>
+          <NotFoundPage path="/NotFoundPage"/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
---
+++ src/components/not-found-page/not-found-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      &lt;&gt;
+      <h1>Страница Не найдена</h1>
+      <a>Перейти на главную страницу</a>
+    </div>
+  );
+};
+
+export default NotFoundPage;
